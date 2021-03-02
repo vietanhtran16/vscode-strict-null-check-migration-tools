348182f4ad413367dbf54a4788f5c71c1b67ad4c
Viet Anh Tran <tranvietanh1996@gmail.com>
2021-03-02 21:38:43 +1300

    WIP

--- autoAdd.js
+++ autoAdd.js
@@ -1,40 +1,31 @@
-
 // @ts-check
 const path = require('path');
 const fs = require('fs');
 const child_process = require('child_process');
 const config = require('./src/config');
 const { forStrictNullCheckEligibleFiles } = require('./src/getStrictNullCheckEligibleFiles');
-
 const vscodeRoot = path.join(process.cwd(), process.argv[2]);
 const srcRoot = path.join(vscodeRoot, 'src');
-
+const tsc = path.join(vscodeRoot, 'node_modules', 'typescript', 'bin', 'tsc');
 const buildCompletePattern = /Found (\d+) errors?\. Watching for file changes\./gi;
-
 forStrictNullCheckEligibleFiles(vscodeRoot, () => { }).then(async (files) => {
-    const tsconfigPath = path.join(srcRoot, config.targetTsconfig);
-
-    const child = child_process.spawn('tsc', ['-p', tsconfigPath, '--watch']);
+    const tsconfigPath = path.join(vscodeRoot, config.targetTsconfig);
+    const child = child_process.spawn(tsc, ['-p', tsconfigPath, '--watch']);
     for (const file of files) {
         await tryAutoAddStrictNulls(child, tsconfigPath, file);
     }
     child.kill();
 });
-
 function tryAutoAddStrictNulls(child, tsconfigPath, file) {
     return new Promise(resolve => {
-        const relativeFilePath = path.relative(srcRoot, file);
+        const relativeFilePath = path.relative(vscodeRoot, file);
         console.log(`Trying to auto add '${relativeFilePath}'`);
-
         const originalConifg = JSON.parse(fs.readFileSync(tsconfigPath).toString());
         originalConifg.files = Array.from(new Set(originalConifg.files.sort()));
-
         // Config on accept
         const newConfig = Object.assign({}, originalConifg);
         newConfig.files = Array.from(new Set(originalConifg.files.concat('./' + relativeFilePath).sort()));
-
         fs.writeFileSync(tsconfigPath, JSON.stringify(newConfig, null, '\t'));
-
         const listener = (data) => {
             const textOut = data.toString();
             const match = buildCompletePattern.exec(textOut);
--- src/tsHelper.js
+++ src/tsHelper.js
@@ -2,30 +2,49 @@
 const path = require('path');
 const ts = require('typescript');
 const fs = require('fs');
-
 module.exports.getImportsForFile = function getImportsForFile(file, srcRoot) {
     const fileInfo = ts.preProcessFile(fs.readFileSync(file).toString());
     return fileInfo.importedFiles
         .map(importedFile => importedFile.fileName)
-        .filter(fileName => !/^vs\/css!/.test(fileName)) // remove css imports
+        .filter(fileName => !/.\.css$/.test(fileName)) // remove css imports
+        .filter(fileName => !/.\.json$/.test(fileName)) // remove css imports
         .filter(x => /\//.test(x)) // remove node modules (the import must contain '/')
+        .filter(filename => !filename.startsWith("@serko")
+            && !filename.startsWith("@zeno")
+            && !filename.startsWith('recompose')
+            && !filename.startsWith('@material')
+            && !filename.startsWith('@storybook')
+            && !filename.startsWith('react-day-picker')
+            && !filename.startsWith('@splitsoftware')
+            && !filename.startsWith('lodash')
+            && !filename.startsWith('date-fns')
+            && !filename.startsWith('rxjs')
+            && !filename.startsWith('react-intl')
+            && !filename.startsWith('moment')
+        )
         .map(fileName => {
             if (/(^\.\/)|(^\.\.\/)/.test(fileName)) {
                 return path.join(path.dirname(file), fileName);
             }
-            if (/^vs/.test(fileName)) {
+            if (/\//.test(fileName)) {
                 return path.join(srcRoot, fileName);
             }
             return fileName;
         }).map(fileName => {
+            if (fs.existsSync(`${fileName}.tsx`)) {
+                return `${fileName}.tsx`;
+            }
             if (fs.existsSync(`${fileName}.ts`)) {
                 return `${fileName}.ts`;
+            }
+            if (fs.existsSync(`${fileName}`)) {
+                return `${fileName}/index.ts`;
             }
             if (fs.existsSync(`${fileName}.js`)) {
                 return `${fileName}.js`;
             }
-            if (fs.existsSync(`${fileName}.d.ts`)) {
-                return `${fileName}.d.ts`;
+            if (fs.existsSync(`${fileName}.d.tsx`)) {
+                return `${fileName}.d.tsx`;
             }
             throw new Error(`Unresolved import ${fileName} in ${file}`);
         });
